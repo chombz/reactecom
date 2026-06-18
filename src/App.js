c196ee82d5c67b6fc161d3e1e9072be0da31a2a1
chombz <chombareuben@gmail.com>
2026-06-19 00:28:27 +0300

    backroomz_syncing

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 
 
-axios.defaults.withCredentials = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+axios.defaults.baseURL = "https://chombzshopz-backroomz-production.up.railway.app/api"; // Set your backend____
+
 
 // Add request interceptor to include auth token
 axios.interceptors.request.use(
